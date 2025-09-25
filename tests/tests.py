d4fe778a3a4b4b101aa7b574a058daa5f3ea4f3c
HaleySchuhl <haleyschuhl@gmail.com>
2025-09-25 16:53:28 -0500

    run all notebooks in CI testing

--- tests/tests.py
+++ tests/tests.py
@@ -20,3 +20,60 @@ def test_notebook(tmpdir):
         nbformat.write(nb, f)
 
     assert os.path.exists(out_nb)
+
+
+def test_notebook2(tmpdir):
+    """Test the notebook."""
+    tmp = tmpdir.mkdir('sub')
+    # Open the notebook
+    with open("index_plm_tutorial_2.ipynb", "r") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Process the notebook
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
+
+    # Save the executed notebook
+    out_nb = os.path.join(tmp, "executed_notebook.ipynb")
+    with open(out_nb, "w", encoding="utf-8") as f:
+        nbformat.write(nb, f)
+
+    assert os.path.exists(out_nb)
+    
+
+def test_notebook3(tmpdir):
+    """Test the notebook."""
+    tmp = tmpdir.mkdir('sub')
+    # Open the notebook
+    with open("index_plm_tutorial_3.ipynb", "r") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Process the notebook
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
+
+    # Save the executed notebook
+    out_nb = os.path.join(tmp, "executed_notebook.ipynb")
+    with open(out_nb, "w", encoding="utf-8") as f:
+        nbformat.write(nb, f)
+
+    assert os.path.exists(out_nb)
+    
+    
+def test_notebook4(tmpdir):
+    """Test the notebook."""
+    tmp = tmpdir.mkdir('sub')
+    # Open the notebook
+    with open("index_plm_tutorial_4.ipynb", "r") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Process the notebook
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
+
+    # Save the executed notebook
+    out_nb = os.path.join(tmp, "executed_notebook.ipynb")
+    with open(out_nb, "w", encoding="utf-8") as f:
+        nbformat.write(nb, f)
+
+    assert os.path.exists(out_nb)
