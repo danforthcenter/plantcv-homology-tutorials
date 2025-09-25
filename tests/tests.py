6bd1f0074e14a7e09ec15f1e4af483f50cf19335
HaleySchuhl <haleyschuhl@gmail.com>
2025-09-25 16:29:56 -0500

    update tests.py to execute a notebook with code

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,7 @@ def test_notebook(tmpdir):
     """Test the notebook."""
     tmp = tmpdir.mkdir('sub')
     # Open the notebook
-    with open("index.ipynb", "r") as f:
+    with open("index_plm_tutorial_1.ipynb", "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Process the notebook
